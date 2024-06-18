1818aeafcffa78684aa18c6a79f66df3e89185db
Dan Evans <danevans@mabr.com>
2024-06-18 13:38:41 -0600

    Initial commit

--- wdd230/scripts/getDates.js
+++
@@ -1,9 +0,0 @@
-// Get the current year
-let currentDate = new Date();
-let ydate = currentDate.getFullYear();
-
-// Find the element with the id "currentYear"
-let currentYearElement = document.getElementById("ydate");
-
-// Update the content of the element with the current year
-currentYearElement.textContent = ydate;
