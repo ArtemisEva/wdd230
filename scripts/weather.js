465ae9ff7a4c9418431bf1f25a03b6fa1fcebe99
Dan Evans <danevans@mabr.com>
2024-07-08 17:54:15 -0600

    Initial commit

---
+++ scripts/weather.js
@@ -0,0 +1,40 @@
+const myTown = document.querySelector('#town');
+const myDescription = document.querySelector('#description');
+const myTemperature = document.querySelector('#temperature');
+const myGraphic = document.querySelector('#graphic');
+
+const myKey = "4ba46795abf8ff493d1bb539c3ab46c5";
+const myLat = 43.9274137
+const myLon = -112.206110
+
+const myURL = `//api.openweathermap.org/data/2.5/weather?lat=${myLat}&lon=${myLon}&appid=${myKey}&units=imperial`
+
+
+async function apiFetch() {
+    try {
+        const response = await fetch(myURL);
+        if (response.ok) {
+            const data = await response.json();
+            displayResults(data);
+        } else {
+            throw Error(await response.text());
+        }
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+
+
+function displayResults(data) {
+    myTown.innerHTML = data.name
+    myDescription.innerHTML = data.weather[0].description
+    myTemperature.innerHTML = `${data.main.temp}&deg;F`
+    const iconsrc = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+    myGraphic.setAttribute('SRC', iconsrc)
+    myGraphic.setAttribute("alt", data.weather[0].description)
+    captionDesc.textContent = `${desc}`;
+}
+
+
+apiFetch();
